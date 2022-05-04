af60d4f23b7f4bf0e970220a8c73d018975f8d49
Ethan <ethanb1@cpp.edu>
2022-05-04 11:05:31 -0700

    Implements user key function and message prompts

---
+++ decrypt.asm
@@ -0,0 +1,38 @@
+#Adds 3 to the ascii value of inutted user stirng
+
+.data
+userString: .space 256
+resString: .space 256
+
+
+.text
+la $a3, resString
+li $v0, 8
+la $a0, userString
+li $a1, 256
+syscall   #get string 1
+
+la $a0, userString #Load address of space into a0
+jal strLength
+
+la $a0, resString
+li $v0, 4                 # print_string
+syscall
+j exit
+
+
+strLength:
+lb $t0, ($a0)
+beq $t0, $zero, endStrLength
+move $t1, $t0
+subi $t1, $t1, 3 #Increments ascii
+sb $t1, 0($a3)
+addi $a3, $a3, 1
+addi $a0, $a0, 1 #Increments position in the array
+j strLength
+endStrLength:
+jr $ra
+
+exit:
+li $v0, 10
+syscall
--- encrypt.asm
+++ encrypt.asm
@@ -1,11 +1,27 @@
 #Adds 3 to the ascii value of inutted user stirng
 
 .data
+msgKey: .asciiz "Please input your desired key length: "
+msgString: .asciiz "Please input your plaintext: "
+msgResult: .asciiz "The result is: "
 userString: .space 256
 resString: .space 256
-
+key: .word 0
 
 .text
+la $a0, msgKey
+li $v0, 4 #Prints the message to prompt the user for desired key length
+syscall
+
+li $v0, 5 #Read key length from the user
+syscall
+
+move $s1, $v0 #$s1, holds the key length
+
+la $a0, msgString
+li $v0, 4	#Prints the message to input plaintext
+syscall
+
 la $a3, resString
 li $v0, 8
 la $a0, userString
@@ -15,9 +31,7 @@ syscall   #get string 1
 la $a0, userString #Load address of space into a0
 jal strLength
 
-la $a0, resString
-li $v0, 4                 # print_string
-syscall
+
 j exit
 
 
@@ -25,7 +39,7 @@ strLength:
 lb $t0, ($a0)
 beq $t0, $zero, endStrLength
 move $t1, $t0
-addi $t1, $t1, 3 #Increments ascii
+add $t1, $t1, $s1 #Increments ascii
 sb $t1, 0($a3)
 addi $a3, $a3, 1
 addi $a0, $a0, 1 #Increments position in the array
@@ -34,5 +48,13 @@ endStrLength:
 jr $ra
 
 exit:
+la $a0, msgResult
+li $v0, 4 #Prits the result message
+syscall
+
+la $a0, resString
+li $v0, 4                 # print_string
+syscall
+
 li $v0, 10
 syscall
