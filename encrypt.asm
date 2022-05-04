963d56b3995bd213d719aa50f5ed1c1b8eb3f7ae
Ethan <ethanb1@cpp.edu>
2022-05-03 17:00:07 -0700

    Adds 3 to the ASCII value of each character inputted by the user

--- encrypt.asm
+++ encrypt.asm
@@ -1,33 +1,37 @@
-#Return the lenght of the inputted string from user
+#Adds 3 to the ascii value of inutted user stirng
 
 .data
 userString: .space 256
+resString: .space 256
 
 
 .text
-li $v0,8
+la $a3, resString
+li $v0, 8
 la $a0, userString
-li $a1, 254
+li $a1, 256
 syscall   #get string 1
 
 la $a0, userString #Load address of space into a0
-jal strLenCheck
+jal strLength
 
-
-move $a0, $v0
-li $v0, 1
+la $a0, resString
+li $v0, 4                 # print_string
 syscall
 j exit
 
-strLenCheck:
-li $v0, -2
-strLength: 
-lb $t0, ($a0)
-addi $a0, $a0, 1
-addi $v0, $v0, 1
-bne $t0, $zero, strLength
-jr $ra
 
+strLength:
+lb $t0, ($a0)
+beq $t0, $zero, endStrLength
+move $t1, $t0
+addi $t1, $t1, 3 #Increments ascii
+sb $t1, 0($a3)
+addi $a3, $a3, 1
+addi $a0, $a0, 1 #Increments position in the array
+j strLength
+endStrLength:
+jr $ra
 
 exit:
 li $v0, 10
