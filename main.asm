61feca92747269ffd3ba36cb3584d86c40f9d259
joshhkim02 <joshhkim02@gmail.com>
2022-05-19 22:06:37 -0700

    Cleaned up code, final version finished

--- main.asm
+++ main.asm
@@ -1,7 +1,6 @@
 # CS 2640 Final Project
 # Caesar Cipher
 # Matthew Alcasabas, Josh Kim, Ethan Bautista, Renard Pascual
-# To-do: fix output: need to ignore spaces
 
 .data
 userString:	.space 	256
@@ -11,7 +10,6 @@ cipherPrompt:	.asciiz "Press '1' to encrypt or '2' to decrypt: "
 keyMsg:		.asciiz "Please input your desired key length: "
 resMsg:		.asciiz "Result: "
 contMsg:	.asciiz "\nPress '1' to continue or '0' to exit: "
-errMsg1:	.asciiz "Error: string must only contain letters.\n"
 
 .text
 main:
@@ -20,43 +18,40 @@ main:
 	li $v0, 4
 	syscall
 	
-	# get user string
+	# Get user string
 	la $v0, 8		
 	la $a0, userString 			# Load 256 bytes of space for the userInput
 	la $a1, 254 				# Read from the first 254 bytes of the string inputted by user
 	syscall
 	
-	# verify user string (call checkWord here!)
-	
-	
-	# reserve space for the result string
+	# Reserve space for the result string
 	la $a3, resString
 	
-	# prompt for key length
+	# Prompt for key length
 	la $a0, keyMsg
 	li $v0, 4
 	syscall
 	
-	# get key length
+	# Get key length
 	li $v0, 5
 	syscall
-	move $s1, $v0				# $s1 = user key length
+	move $s1, $v0				# $s1 = User key length
 	
-	# ask for encrypt or decrypt
+	# Ask for encrypt or decrypt
 	promptMode:
 	la $a0, cipherPrompt
 	li $v0, 4
 	syscall
 	
-	# get user choice
+	# Get user choice
 	li $v0, 5
 	syscall
-	move $s0, $v0				# $s0 = user's choice
+	move $s0, $v0				# $s0 = User's choice
 	
-	# does user want to encrypt or decrypt?
-	beq $s0, 1, enc				# if $s0 = 1, encrypt string
-	beq $s0, 2, dec				# if $s0 = 2, decrypt string
-	j promptMode				# if anything else, ask for choice again
+	# Does user want to encrypt or decrypt?
+	beq $s0, 1, enc				# If $s0 = 1, encrypt string
+	beq $s0, 2, dec				# If $s0 = 2, decrypt string
+	j promptMode				# If anything else, ask for choice again
 	
 	enc:
 		la $a0, userString 		# Load address of space into a0
@@ -73,7 +68,7 @@ main:
 print:
 	# print "result: "
 	la $a0, resMsg
-	li $v0, 4 				# Prits the result message
+	li $v0, 4 				# Prints the result message
 	syscall
 
 	# print the resulting string
@@ -81,113 +76,53 @@ print:
 	li $v0, 4				# print_string
 	syscall
 	
-	# ask user if they want to run the program again
+	# Ask user if they want to run the program again
 	promptContinue:
 	la $a0, contMsg
 	li $v0, 4
 	syscall
 	
-	# get user choice
+	# Get user choice
 	li $v0, 5
 	syscall
 	move $s4, $v0
 	
-	# does user want to continue?
+	# Does user want to continue?
 	beq $s4, 0, exit
 	beq $s4, 1, main
 	j promptContinue
 	
 exit:	
-	# gracefully exit program
+	# Gracefully exit program
 	li $v0, 10
 	syscall
 	
 strEncrypt:
-	lb $t0, ($a0)				# load nth byte from userString (byte = current ascii char)
-	beq $t0, $zero, endStrEncrypt		# if byte == 0, end loop (0 = null, end of string)
-	move $t1, $t0				# copy current byte to $t1
-	beq $t1, 32, skipEncrypt		# if the char is a space (ascii 32), don't do anything to the ascii value
-	beq $t1, 0, skipEncrypt			# if the char is a null (ascii 0), don't do anything to the ascii value
+	lb $t0, ($a0)				# Load n'th byte from userString (byte = current ascii char)
+	beq $t0, $zero, endStrEncrypt		# If byte == 0, end loop (0 = null, end of string)
+	move $t1, $t0				# Copy current byte to $t1
+	beq $t1, 32, skipEncrypt		# If the char is a space (ascii 32), don't do anything to the ascii value
+	beq $t1, 0, skipEncrypt			# If the char is a null (ascii 0), don't do anything to the ascii value
 	add $t1, $t1, $s1 			# Increments ascii by specified key length
-	skipEncrypt:				# char is a space/null. leave the ascii value alone
-	sb $t1, 0($a3)				# store the current byte into the resString
-	addi $a3, $a3, 1			# increment resString address (reserve more space for string)
+	skipEncrypt:				# Char is a space/null; Leave the ascii value alone
+	sb $t1, 0($a3)				# Store the current byte into the resString
+	addi $a3, $a3, 1			# Increment resString address (reserve more space for string)
 	addi $a0, $a0, 1 			# Increments position in the array
-	j strEncrypt				# loop back to beginning of function
+	j strEncrypt				# Loop back to beginning of function
 	endStrEncrypt:
-	jr $ra					# return control to caller
+	jr $ra					# Return control to caller
 
 strDecrypt:
-	lb $t0, ($a0)				# load nth byte from userString
-	beq $t0, $zero, endStrDecrypt		# if byte == 0, end loop (0 = null, end of string)
-	move $t1, $t0				# copy current byte to $t1
-	beq $t1, 32, skipDecrypt		# if the char is a space (ascii 32), don't do anything to the ascii value
-	beq $t1, 0, skipDecrypt			# if the char is a null (ascii 0), don't do anything to the ascii value
-	sub $t1, $t1, $s1 			# decrements ascii by specified key length
-	skipDecrypt:				# char is a space. leave the ascii value alone
-	sb $t1, 0($a3)				# store current byte into the resString
-	addi $a3, $a3, 1			# increment resString address (reserve more space for string)
+	lb $t0, ($a0)				# Load nth byte from userString
+	beq $t0, $zero, endStrDecrypt		# If byte == 0, end loop (0 = null, end of string)
+	move $t1, $t0				# Copy current byte to $t1
+	beq $t1, 32, skipDecrypt		# If the char is a space (ascii 32), don't do anything to the ascii value
+	beq $t1, 0, skipDecrypt			# If the char is a null (ascii 0), don't do anything to the ascii value
+	sub $t1, $t1, $s1 			# Decrements ascii by specified key length
+	skipDecrypt:				# Char is a space; Leave the ascii value alone
+	sb $t1, 0($a3)				# Store current byte into the resString
+	addi $a3, $a3, 1			# Increment resString address (reserve more space for string)
 	addi $a0, $a0, 1 			# Increments position in the array
-	j strDecrypt				# loop back to beginning of function
+	j strDecrypt				# Loop back to beginning of function
 	endStrDecrypt:
-	jr $ra					# return control to caller
-	
-checkWord:
-	# this function should be run in a loop that iterates thru the user's string.
-	# returns a 0 (false) if it finds an invalid char, returns a 1 (true) if it finds a valid char.
-	
-	# ensure ascii values are between 65 and 90. if they are, we are ok to proceed
-   	bge $a0, 65, endCheckWord		# ascii 65 = A = valid char
-    	ble $a0, 90, endCheckWord		# ascii 90 = Z = valid char
-    	
-    	# if ascii values are lowercase (between 97 and 122), convert the char to uppercase
-    	bge $a0, 97, toUpperCase		# ascii 97 = a = make this uppercase
-    	ble $a0, 122, toUpperCase		# ascii 122 = z = make this uppercase
-    	
-    	toUpperCase:
-    		subi $a0, $a0, 32		# subtracting 32 from the ascii value of a lowercase will convert it to uppercase
-    		j endCheckWord    		# letter should be ok
-    	
-    	# on failure
-    	li $v0, 0				# if function returns a 0, an invalid char was found, tell the user and have them try again
-    	jr $ra
-
-	endCheckWord:
-    		li $v0, 1			# if returns a 1, the current char is valid, continue iterating thru loop
-    		jr $ra
-    		
-checkNumber:
-	
-
-# checkUserString:
-	# this function will check userString to ensure:
-	# 1. ascii values stay between 65 and 90 (i.e. all uppercase chars)
-	#	if it finds a lowercase char, convert it to uppercase (subtract 32)
-	# 2. no illegal chars are inputted (e.g. { } | \ , . / ? etc.)
-	#	if it finds something that isn't a letter, print an error msg and have user try again
-	
-	# iterate thru usrString at $a0 to check each character
-	# no need to check resString, because resString should not have any illegal chars after running it thru checkBounds
-	
-# checkBounds:
-	# this function will check the ascii value and ensure that it is between 65(A) and 90(Z)
-	# this ensures that no weird chars are printed in the encrypted/decrypted output
-	# renard already has the algorithm for this, he just needs to implement it in MIPS asm
-	
-mod:
-	# move args into temp registers
-	move $t0, $a0
-	move $t1, $a1
-	
-	# divide the two numbers
-	div $t0, $t1
-	
-	# remainder stored in hi register, move remainder to $v0
-	mfhi $v0
-	
-	# return control to caller
-	jr $ra
-
-	
-	
-	
+	jr $ra					# Return control to caller
